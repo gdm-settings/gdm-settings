e003da0bf5fb21949c638a91080e704ba7711e18
Mazhar Hussain <mmazharhussainkgb1145@gmail.com>
2021-09-01 16:23:11 +0500

    Added: functions.py

---
+++ src/functions.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+from get_theme_list import ThemeList
+
+def set_background(args):
+    pass
+def set_theme(args):
+    pass
+def list_themes(args):
+    print(*ThemeList, sep='\n')
+def list_colors(args):
+    pass
+def backup_update(args):
+    pass
+def backup_restore(args):
+    pass
+def extract_default_theme(args):
+    pass
+def show_manual(args):
+    pass
+def show_examples(args):
+    pass
