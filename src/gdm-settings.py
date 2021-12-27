1b123091c37ad08338ca46f53aa0c0a69e008190
Mazhar Hussain <mmazharhussainkgb1145@gmail.com>
2021-12-27 19:50:37 +0500

    Reorganize Code

--- src/gdm-settings.py
+++ src/gdm-settings.py
@@ -24,32 +24,35 @@ class MyApplication(Gtk.Application):
         self.set_application_id("org.gnome.gdm-settings")
         
     def assign_members(self):
-        self.builder = Gtk.Builder.new_from_file(os.path.join(script_dir, "main-window.ui"))
-        self.win = self.builder.get_object("window_main")
+        main_window_ui_file = os.path.join(script_dir, "main-window.ui")
+        self.builder = Gtk.Builder.new_from_file(main_window_ui_file)
+        self.window_main = self.builder.get_object("window_main")
         self.combobox = self.builder.get_object("combobox")
         self.refresh_combobox_entries(self.combobox)
+        self.dialog_error = self.builder.get_object("dialog_error")
 
     def connect_signals(self):
         self.connect("activate", self.on_activate)
         self.builder.get_object("button_quit").connect("clicked", self.destroy_window)
         self.builder.get_object("button_set_theme").connect("clicked", self.call_set_theme)
+        self.dialog_error.connect("response", self.hide_dialog)
         #self.combobox.connect("popup", self.refresh_combobox_entries)
         
     def on_activate(self, something=None):
-        self.add_window(self.win)
-        self.win.present()
+        self.add_window(self.window_main)
+        self.window_main.present()
 
     def print_button_label(self, button):
         print(button.get_label())
     
     def refresh_combobox_entries(self, combobox):
-        self.combobox.remove_all()
+        combobox.remove_all()
         for theme in get_theme_list():
-            self.combobox.append_text(theme)
-        self.combobox.remove(0)
+            combobox.append_text(theme)
+        combobox.remove(0)
         
     def destroy_window(self, widget=None):
-        self.win.destroy()
+        self.window_main.destroy()
 
     def hide_dialog(self, widget, *args):
         widget.hide()
@@ -59,9 +62,7 @@ class MyApplication(Gtk.Application):
         try:
             set_theme(theme=selected_theme, askpass="pkexec")
         except TypeError:
-            dialog = self.builder.get_object("dialog_error")
-            dialog.connect("response", self.hide_dialog)
-            dialog.present()
+            self.dialog_error.present()
 
 if __name__ == '__main__':
     MyApplication().run()
