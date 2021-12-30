0b7409f4a58aea91b88d12fe024be8f18328e8a2
Mazhar Hussain <mmazharhussainkgb1145@gmail.com>
2021-12-30 20:42:43 +0500

    gdm-settings.py: code cleanup (mostly rewrite)

--- src/gdm-settings.py
+++ src/gdm-settings.py
@@ -1,68 +1,60 @@
 #!/usr/bin/env python3
 import gi, sys, os.path
+
 gi.require_version("Gtk", '4.0')
-gi.require_version("Gio", '2.0')
 from gi.repository import Gtk
 
 from functions import get_theme_list, set_theme
 
-# Empty class to store arbitrary function arguments
 script_realpath = os.path.realpath(sys.argv[0])
 script_basename = os.path.basename(script_realpath)
 script_dir = os.path.dirname(script_realpath)
+main_window_ui_file = os.path.join(script_dir, "main-window.ui")
 
+# Empty Class+Object to contain widgets
+class WidgetContainer:
+    pass
+widgets = WidgetContainer()
 
-class MyApplication(Gtk.Application):
-    def __init__(self) -> None:
-        super().__init__()
-        self.assign_members()
-        self.connect_signals()
-        self.set_properties()
-        
-    def set_properties(self):
-        #print(*dir(self.props), sep="\n)")
-        self.set_application_id("org.gnome.gdm-settings")
-        
-    def assign_members(self):
-        main_window_ui_file = os.path.join(script_dir, "main-window.ui")
-        self.builder = Gtk.Builder.new_from_file(main_window_ui_file)
-        self.window_main = self.builder.get_object("window_main")
-        self.combobox = self.builder.get_object("combobox")
-        self.refresh_combobox_entries(self.combobox)
-        self.dialog_error = self.builder.get_object("dialog_error")
+def load_widgets():
+    # Initialize Builder
+    widgets.builder = Gtk.Builder.new_from_file(main_window_ui_file)
+    # Get Widgets from builder
+    widgets.window_main = widgets.builder.get_object("window_main")
+    widgets.dialog_error = widgets.builder.get_object("dialog_error")
+    widgets.combobox = widgets.builder.get_object("combobox")
+    widgets.button_quit = widgets.builder.get_object("button_quit")
+    widgets.button_set_theme = widgets.builder.get_object("button_set_theme")
 
-    def connect_signals(self):
-        self.connect("activate", self.on_activate)
-        self.builder.get_object("button_quit").connect("clicked", self.destroy_window)
-        self.builder.get_object("button_set_theme").connect("clicked", self.call_set_theme)
-        self.dialog_error.connect("response", self.hide_dialog)
-        #self.combobox.connect("popup", self.refresh_combobox_entries)
-        
-    def on_activate(self, something=None):
-        self.add_window(self.window_main)
-        self.window_main.present()
+def call_set_theme(widget=None):
+    selected_theme = widgets.combobox.get_active_text()
+    if selected_theme:
+        set_theme(theme=selected_theme, askpass="pkexec")
+    else:
+        widgets.dialog_error.present()
 
-    def print_button_label(self, button):
-        print(button.get_label())
-    
-    def refresh_combobox_entries(self, combobox):
-        combobox.remove_all()
-        for theme in get_theme_list():
-            combobox.append_text(theme)
-        combobox.remove(0)
-        
-    def destroy_window(self, widget=None):
-        self.window_main.destroy()
+def on_activate(app):
+    # Load Widgets
+    load_widgets()
 
-    def hide_dialog(self, widget, *args):
-        widget.hide()
-        
-    def call_set_theme(self, widget=None):
-        selected_theme = self.combobox.get_active_text()
-        if selected_theme:
-            set_theme(theme=selected_theme, askpass="pkexec")
-        else:
-            self.dialog_error.present()
+    # Add Theme List to combobox
+    for theme in get_theme_list():
+        widgets.combobox.append_text(theme)
+    widgets.combobox.remove(0)
+
+    # Connect Signals
+    widgets.button_quit.connect("clicked", lambda x: widgets.window_main.close())
+    widgets.button_set_theme.connect("clicked", call_set_theme)
+    widgets.dialog_error.connect("response", lambda x,y: widgets.dialog_error.hide())
+
+    # Add Window to app
+    app.add_window(widgets.window_main)
+
+    # Show Window
+    widgets.window_main.present()
+
 
 if __name__ == '__main__':
-    MyApplication().run()
+    app = Gtk.Application(application_id="org.gtk.gdm-settings")
+    app.connect("activate", on_activate)
+    app.run(sys.argv)
