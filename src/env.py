2a28f4387fd5e1dd44bace435fe33150c7c9b0a2
Mazhar Hussain <realmazharhussain@gmail.com>
2022-04-13 22:10:52 +0500

    import os.environ as itself and not as env

--- src/env.py
+++ src/env.py
@@ -1,20 +1,20 @@
-from os import environ as env
+from os import environ
 from enum import Enum
 from .utils import PATH
 
 PackageType = Enum('PackageType', ['NONE', 'AppImage', 'Flatpak'])
 InterfaceType = Enum('InterfaceType', ['NONE', 'Graphical', 'CommandLine'])
 
-HOME = env.get('HOME')
-XDG_CACHE_HOME = env.get('XDG_CACHE_HOME', f"{HOME}/.cache")
-SYSTEM_DATA_DIRS = PATH(env.get('SYSTEM_DATA_DIRS', '/usr/local/share:/usr/share'))
+HOME = environ.get('HOME')
+XDG_CACHE_HOME = environ.get('XDG_CACHE_HOME', f"{HOME}/.cache")
+SYSTEM_DATA_DIRS = PATH(environ.get('SYSTEM_DATA_DIRS', '/usr/local/share:/usr/share'))
 
 INTERFACE_TYPE = InterfaceType.NONE
 
 PACKAGE_TYPE = PackageType.NONE
 HOST_ROOT = ''
-if env.get('FLATPAK_ID'): # Flatpak
+if environ.get('FLATPAK_ID'): # Flatpak
     PACKAGE_TYPE = PackageType.Flatpak
     HOST_ROOT = '/var/run/host'
-elif env.get('APPDIR'):   # AppImage
+elif environ.get('APPDIR'):   # AppImage
     PACKAGE_TYPE = PackageType.AppImage
