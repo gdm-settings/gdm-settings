eeb9f37a8d362ba2d69323d5daf922b2dffb9db9
Mazhar Hussain <realmazharhussain@gmail.com>
2022-04-17 11:29:02 +0500

    fix: syntax error in display-page.blp and update comments for Translators

--- po/update-pot-file.sh
+++ po/update-pot-file.sh
@@ -6,4 +6,4 @@ POTFILES=(data{,/gschemas}/application_id.* src/*.py*)
 BLPFILES=(src/blueprints/*.blp*)
 rm -f "$pot_file"
 xgettext --add-comments=Translators -o "$pot_file" "${POTFILES[@]}"
-xgettext --add-comments=Translators -j -L Python -o "$pot_file" "${BLPFILES[@]}"
+xgettext --add-comments=Translators -j -L ObjectiveC --from-code=UTF-8 -o "$pot_file" "${BLPFILES[@]}"
