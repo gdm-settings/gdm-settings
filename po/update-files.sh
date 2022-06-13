3a11a7252f343e6c8b31cb5400de8eaad6942e2c
Mazhar Hussain <realmazharhussain@gmail.com>
2022-06-13 11:17:25 +0500

    Rename update-pot-file.sh to update-files.sh & Now, it updates all .po files too

--- po/update-files.sh
+++ po/update-files.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 progDir=$(dirname "$(realpath "$0")")
-pot_file="$progDir"/gdm-settings.pot
 cd "$progDir"/..
+
+pot_file=po/gdm-settings.pot
 POTFILES=(data{,/gschemas}/application_id.* src/*.py*)
 BLPFILES=(src/blueprints/*.blp*)
+
+echo "Updating '$pot_file' ..."
 rm -f "$pot_file"
 xgettext --add-comments=Translators -o "$pot_file" "${POTFILES[@]}"
 xgettext --add-comments=Translators -j -L ObjectiveC --from-code=UTF-8 -o "$pot_file" "${BLPFILES[@]}"
+
+for po_file in po/*.po
+do
+  echo "Updating '$po_file' ..."
+  msgmerge -U --backup=off "$po_file" "$pot_file" 2>/dev/null
+done
