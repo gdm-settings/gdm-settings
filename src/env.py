a60994a94de4baf63e303b56b8f920bb7c2e637b
Mazhar Hussain <realmazharhussain@gmail.com>
2022-06-20 22:04:16 +0500

    Prettify env.py

--- src/env.py
+++ src/env.py
@@ -4,21 +4,21 @@ from os import environ
 from enum import Enum
 from .utils import PATH
 
-PackageType = Enum('PackageType', ['NONE', 'AppImage', 'Flatpak'])
+PackageType   = Enum('PackageType',   ['NONE', 'AppImage', 'Flatpak'])
 InterfaceType = Enum('InterfaceType', ['NONE', 'Graphical', 'CommandLine'])
 
-HOME = environ.get('HOME')
-XDG_CACHE_HOME = environ.get('XDG_CACHE_HOME', f"{HOME}/.cache")
-XDG_DATA_HOME = PATH(environ.get('XDG_DATA_HOME', '{HOME}/.local/share'))
-XDG_DATA_DIRS = PATH(environ.get('XDG_DATA_DIRS', '/usr/local/share:/usr/share'))
+HOME             =      environ.get('HOME')
+XDG_CACHE_HOME   =      environ.get('XDG_CACHE_HOME',  f'{HOME}/.cache')
+XDG_DATA_HOME    = PATH(environ.get('XDG_DATA_HOME',   f'{HOME}/.local/share'))
+XDG_DATA_DIRS    = PATH(environ.get('XDG_DATA_DIRS',    '/usr/local/share:/usr/share'))
 SYSTEM_DATA_DIRS = PATH(environ.get('SYSTEM_DATA_DIRS', '/usr/local/share:/usr/share'))
 
 INTERFACE_TYPE = InterfaceType.NONE
 
 PACKAGE_TYPE = PackageType.NONE
-HOST_ROOT = ''
+HOST_ROOT    = ''
 if environ.get('FLATPAK_ID'): # Flatpak
     PACKAGE_TYPE = PackageType.Flatpak
-    HOST_ROOT = '/var/run/host'
+    HOST_ROOT    = '/var/run/host'
 elif environ.get('APPDIR'):   # AppImage
     PACKAGE_TYPE = PackageType.AppImage
