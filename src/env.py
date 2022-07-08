3605f52ecd082b2c1527ad2abbd67b152906bb9d
Mazhar Hussain <realmazharhussain@gmail.com>
2022-07-08 17:50:32 +0500

    env.XDG_DATA_HOME is not a utils.PATH instance

--- src/env.py
+++ src/env.py
@@ -7,7 +7,7 @@ from .utils import PATH
 # Environment variables
 HOME             =      environ.get('HOME')
 XDG_CACHE_HOME   =      environ.get('XDG_CACHE_HOME',  f'{HOME}/.cache')
-XDG_DATA_HOME    = PATH(environ.get('XDG_DATA_HOME',   f'{HOME}/.local/share'))
+XDG_DATA_HOME    =      environ.get('XDG_DATA_HOME',   f'{HOME}/.local/share')
 XDG_DATA_DIRS    = PATH(environ.get('XDG_DATA_DIRS',    '/usr/local/share:/usr/share'))
 SYSTEM_DATA_DIRS = PATH(environ.get('SYSTEM_DATA_DIRS', '/usr/local/share:/usr/share'))
 
