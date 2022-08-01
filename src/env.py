fe60f4cd09de30d207018350edea5d292c4a94c7
Mazhar Hussain <realmazharhussain@gmail.com>
2022-08-01 11:09:17 +0500

    Remove env.XDG_DATA_HOME (never used)

--- src/env.py
+++ src/env.py
@@ -7,7 +7,6 @@ from .utils import PATH
 # Environment variables
 HOME           =      environ.get('HOME')
 XDG_CACHE_HOME =      environ.get('XDG_CACHE_HOME',  f'{HOME}/.cache')
-XDG_DATA_HOME  =      environ.get('XDG_DATA_HOME',   f'{HOME}/.local/share')
 XDG_DATA_DIRS  = PATH(environ.get('XDG_DATA_DIRS',    '/usr/local/share:/usr/share'))
 HOST_DATA_DIRS = PATH(os.environ.get('HOST_DATA_DIRS', '/usr/local/share:/usr/share'))
 
