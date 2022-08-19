7ac2026af59dfe8c2b28cf03faaef98e16209d78
Mazhar Hussain <realmazharhussain@gmail.com>
2022-08-19 15:31:02 +0500

    Run 'Extract Default Shell Theme' in background

--- src/pages/tools.py
+++ src/pages/tools.py
@@ -24,6 +24,7 @@ class ToolsPageContent (PageContent):
         self.top_bar_tweaks_switch = self.builder.get_object('top_bar_tweaks_switch')
         self.extract_shell_theme_button = self.builder.get_object('extract_shell_theme_button')
 
+        self.window.task_counter.register(self.extract_shell_theme_button)
         self.extract_shell_theme_button.connect('clicked', self.on_extract_shell_theme)
 
         # Bind to GSettings
@@ -31,7 +32,25 @@ class ToolsPageContent (PageContent):
         bind(self.gsettings, 'top-bar-tweaks', self.top_bar_tweaks_switch, 'active')
 
     def on_extract_shell_theme(self, button):
+        self.window.task_counter.inc()
+        self.extract_shell_theme_async(self.on_extract_shell_theme_finish)
 
+    @staticmethod
+    def on_extract_shell_theme_finish(self, result, user_data):
+        status, theme_name = self.extract_shell_theme_finish(result)
+        self.window.task_counter.dec()
+
+        if status.success:
+            # Translators: Do not translate '{folder}' and '{name}'. Keep these as they are.
+            # They will be replaced by an actual folder path and theme name during runtime.
+            msg = _("Default shell theme extracted to '{folder}' as '{name}'")
+            message = msg.format(folder=ThemesDir, name=theme_name)
+        else:
+            message = _("Failed to extract default theme")
+        toast = Adw.Toast(timeout=2, priority="high", title=message)
+        self.window.toast_overlay.add_toast(toast)
+
+    def extract_shell_theme (self):
         perm_theme_name = 'default-extracted'
         perm_theme_path = os.path.join(ThemesDir, perm_theme_name)
         temp_theme_path = os.path.join(TEMP_DIR, 'extracted-theme')
@@ -49,13 +68,38 @@ class ToolsPageContent (PageContent):
         command_elevator.add(f'cp -rfT {temp_theme_path} {perm_theme_path}')
         status = command_elevator.run()
 
-        # Notify the user via in-app notification
-        if status.success:
-            # Translators: Do not translate '{folder}' and '{name}'. Keep these as they are.
-            # They will be replaced by an actual folder path and theme name during runtime.
-            msg = _("Default shell theme extracted to '{folder}' as '{name}'")
-            message = msg.format(folder=ThemesDir, name=perm_theme_name)
-        else:
-            message = _("Failed to extract default theme")
-        toast = Adw.Toast(timeout=2, priority="high", title=message)
-        self.window.toast_overlay.add_toast(toast)
+        return status, perm_theme_name
+
+    def extract_shell_theme_async(self, callback):
+        '''Run apply_settings asynchronously'''
+
+        task = Gio.Task.new(self, None, callback, None)
+        task.set_return_on_cancel(False)
+
+        task.run_in_thread(self._extract_shell_theme_thread_callback)
+
+    def _extract_shell_theme_thread_callback(self, task, source_object, task_data, cancellable):
+        '''Called by apply_settings_async to run apply_settings in a separate thread'''
+
+        if task.return_error_if_cancelled():
+            return
+
+        try:
+            value = self.extract_shell_theme()
+            task.return_value(value)
+        except Exception as e:
+            task.return_value(e)
+
+    def extract_shell_theme_finish(self, result):
+        '''Returns result(return value) of apply_settings_async'''
+
+        if not Gio.Task.is_valid(result, self):
+            from .utils import ProcessReturnCode
+            return ProcessReturnCode(-1)
+
+        value = result.propagate_value().value
+
+        if isinstance(value, Exception):
+            raise value
+
+        return value
