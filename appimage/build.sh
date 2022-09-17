gdm-settings/gdm-settings

commit 5c1ef93f27ac868e9d8a340f3985a41014a72f8d

Add VERSION info in AppImage

appimage/build.sh
@@ -71,4 +71,6 @@ fi
 echo
 
 
+VERSION_STRING=$("${AppDir}"/usr/bin/gdm-settings --version)
+export VERSION=${VERSION_STRING##* }
 appimagetool "${AppDir}" "${BuildDir}"/${ProjectName}.AppImage

commit a0f0c4671e7d06bf8c0c8ad5c45c22063322e342

Fix "Settings schema '$AppID' not installed" error in appimage/build.sh
Error was introduced in 5c1ef93f27ac868e9d8a340f3985a41014a72f8d

appimage/build.sh
@@ -70,6 +70,6 @@ fi
 echo
 
 
-VERSION_STRING=$("${AppDir}"/usr/bin/gdm-settings --version)
+VERSION_STRING=$("${AppRun}" --version)
 export VERSION=${VERSION_STRING##* }
 appimagetool "${AppDir}" "${BuildDir}"/${ProjectName}.AppImage