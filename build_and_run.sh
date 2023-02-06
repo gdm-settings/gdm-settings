7788b088dc1196c62452e80e5338d0ef08968227
Mazhar Hussain <realmazharhussain@gmail.com>
2023-02-06 19:23:05 +0500

    build_and_run.sh: Remove app_dir before installing to it

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,6 +3,8 @@ repo_dir=$(dirname "$(realpath "$0")")
 build_dir=${repo_dir}/build
 app_dir=${build_dir}/AppDir
 
+rm -rf "${app_dir}"
+
 action=setup
 if test -d "${build_dir}"; then
   action=configure
