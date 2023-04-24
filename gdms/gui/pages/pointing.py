bad30aca1b8250f4316de3dd0e9d601e288156d6
Mazhar Hussain <realmazharhussain@gmail.com>
2023-04-24 21:54:42 +0500

    Merge pointing_utils/cursor.py into pointing.py

--- gdms/gui/pages/pointing.py
+++ gdms/gui/pages/pointing.py
@@ -1,10 +1,10 @@
 from gi.repository import Gtk
 
+from gdms.utils import GProperty
 from gdms.settings import pointing_settings, mouse_settings, touchpad_settings
 from gdms.gui.widgets import SwitchRow
 
 from .common import PageContent
-from .pointing_utils.cursor import CursorSizeSelector
 
 
 class PointingPageContent (PageContent):
@@ -53,3 +53,73 @@ class PointingPageContent (PageContent):
         touchpad_settings.bind('two-finger-scrolling', self.t_two_finger_scrolling_row, 'enabled')
         touchpad_settings.bind('disable-while-typing', self.t_disable_while_typing_row, 'enabled')
         touchpad_settings.bind('speed', self.t_speed_scale.props.adjustment, 'value')
+
+
+@Gtk.Template(resource_path='/app/ui/pointing-page/cursor-size-button.ui')
+class CursorSizeButton (Gtk.ToggleButton):
+    __gtype_name__ = 'CursorSizeButton'
+
+    cursor_size = GProperty(int, default=24, construct_only=True)
+    size_name = GProperty(str)
+
+
+@Gtk.Template(resource_path='/app/ui/pointing-page/cursor-size-selector.ui')
+class CursorSizeSelector (Gtk.ListBoxRow, Gtk.Buildable):
+    __gtype_name__ = 'CursorSizeSelector'
+    box = Gtk.Template.Child()
+
+    def __init__(self):
+        super().__init__()
+
+        self._item_dict = {}
+        self._first_item = None
+        self._selected_size = 0
+        self._selected_name = ''
+
+    @GProperty(int)
+    def selected_size(self):
+        return self._selected_size
+
+    @selected_size.setter
+    def selected_size(self, value):
+        if self._selected_size == value:
+            return
+
+        if value not in self._item_dict:
+            return
+
+        self._selected_size = value
+        btn = self._item_dict[value]
+        btn.set_active(True)
+
+        self._selected_name = btn.size_name
+        self.notify('selected-name')
+
+    @GProperty(str, writable=False)
+    def selected_name(self):
+        return self._selected_name
+
+    def do_add_child(self, builder, child, _type):
+        if not isinstance(child, CursorSizeButton):
+            raise TypeError('Child is a ' + type(child) + '. Should be a CursorSizeButton instead')
+
+        cursor_size = child.cursor_size
+        if cursor_size in self._item_dict:
+            raise ValueError('A CursorSizeButton with cursor-size of ' + cursor_size
+                             + ' has already been added')
+
+        self.box.append(child)
+        child.connect('toggled', self.selection_changed_cb)
+        self._item_dict[cursor_size] = child
+
+        if self._first_item:
+            child.set_group(self._first_item)
+        else:
+            self._first_item = child
+            child.set_active(True)
+
+    def selection_changed_cb(self, child):
+        if not child.get_active() or child.cursor_size == self.selected_size:
+            return
+
+        self.selected_size = child.cursor_size
