gdm-settings/gdm-settings

commit 81eea296a0a6cfabc0a3ee24739af6935843de9b

Log default shell theme GResource file
Signed-off-by: Mazhar Hussain <realmazharhussain@gmail.com>

gdms/gui/__init__.py
@@ -14,7 +14,7 @@ from gdms import stderr_log_handler
 from gdms import env
 from gdms.enums import PackageType
 from gdms.utils import BackgroundTask, GSettings
-from gdms.gresource import ShellGresourceFile, UbuntuGdmGresourceFile
+from gdms import gresource
 from gdms import settings
 
 Gio.Resource.load(APP_DATA_DIR + '/resources.gresource')._register()
@@ -107,8 +107,9 @@ class GdmSettingsApp(Adw.Application):
         logger.info(f"PackageType            = {env.PACKAGE_TYPE.name}")
         logger.info(f"TEMP_DIR               = {env.TEMP_DIR}")
         logger.info(f"HOST_DATA_DIRS         = {env.HOST_DATA_DIRS}")
-        logger.info(f"ShellGresourceFile     = {ShellGresourceFile}")
-        logger.info(f"UbuntuGdmGresourceFile = {UbuntuGdmGresourceFile}")
+        logger.info(f"ShellGresourceFile     = {gresource.ShellGresourceFile}")
+        logger.info(f"UbuntuGdmGresourceFile = {gresource.UbuntuGdmGresourceFile}")
+        logger.info(f"Default Gresource File = {gresource.get_default()}")
 
         self.settings = GSettings(APP_ID)
 

commit 7874ec799a968d728e8e472fe3be53d2e24caffd

Fix a typo caused by 81eea29
Signed-off-by: Mazhar Hussain <realmazharhussain@gmail.com>

gdms/gui/__init__.py
@@ -162,7 +162,7 @@ class GdmSettingsApp(Adw.Application):
 
             return False
 
-        gdm_installed = bool(ShellGresourceFile)
+        gdm_installed = bool(gresource.ShellGresourceFile)
         polkit_installed = check_dependency('pkexec')
 
         if gdm_installed and polkit_installed: