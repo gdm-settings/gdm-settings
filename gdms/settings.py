e4ff6fcc9ac812a37ce525139591d01a3dbfffce
Mazhar Hussain <realmazharhussain@gmail.com>
2023-08-13 10:31:08 +0500

    Small code styling fix

--- gdms/settings.py
+++ gdms/settings.py
@@ -253,7 +253,7 @@ def get_css() -> str:
         background_color = appearance_settings['background-color']
         css += "\n"
         css += ".login-dialog { background: transparent; }\n"
-        css += "#lockDialogGroup { background-color: "+ background_color + "; }\n"
+        css += "#lockDialogGroup { background-color: " + background_color + "; }\n"
 
     ### Top Bar ###
     def select_elem(elem:str='') -> str:
