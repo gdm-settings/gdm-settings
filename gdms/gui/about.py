f803c480f2553d58150946b2f4c9fd365c4a0d14
Mazhar Hussain <realmazharhussain@gmail.com>
2023-12-10 19:45:52 +0500

    Use Adw.AboutWindow.new_from_appdata()
    Can be used for automatic fetching of release notes

--- gdms/gui/about.py
+++ gdms/gui/about.py
@@ -1,9 +1,8 @@
 from gettext import gettext as _, pgettext as C_
 
 from gi.repository import Adw
-from gi.repository import Gtk
 
-from gdms import APP_ID, APP_NAME, VERSION, temp_log_io
+from gdms import VERSION, temp_log_io
 
 
 mazhar_hussain = C_("Name of Developer", "Mazhar Hussain") + " <realmazharhussain@gmail.com>"
@@ -13,28 +12,20 @@ thales_binda   = C_("Name of Artist",    "Thales Bindá") +   " <thales.i.o.b@gm
 def about_window(win):
     temp_log_io.seek(0)
 
-    abt = Adw.AboutWindow(
-        transient_for = win,
-        modal         = True,
-        application_name = APP_NAME,
-        application_icon = APP_ID,
-        license_type     = Gtk.License.AGPL_3_0,
-        version          = VERSION,
-        developer_name   = C_("Name of Developer", "Mazhar Hussain"),
+    abt = Adw.AboutWindow.new_from_appdata("/app/info.xml")
+    abt.set_properties(
+        artists = [thales_binda],
         copyright = _("Copyright 2021-2023 Mazhar Hussain"),
-        website   = "https://gdm-settings.github.io",
         debug_info = temp_log_io.read(),
-        developers  = [mazhar_hussain],
-        documenters = [mazhar_hussain],
-        artists     = [thales_binda],
         debug_info_filename = 'gdm-settings.log',
-
-        support_url = "https://github.com/gdm-settings/gdm-settings/discussions/categories/q-a",
-        issue_url   = "https://github.com/gdm-settings/gdm-settings/issues/new/choose",
+        developers = [mazhar_hussain],
+        documenters = [mazhar_hussain],
+        transient_for = win,
+        version = VERSION,
 
         # Translators: Do not translate this string. Put your info here in the form
         # 'name <email>' including < and > but not quotes.
         translator_credits = _("translator-credits"),
-        )
+    )
 
     return abt
